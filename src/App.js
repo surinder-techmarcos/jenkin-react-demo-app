6cdc484aabcd9617cb660f0f31855e20cc56e9ce
SurinderB <spal@techmarcos.com>
2022-11-06 02:06:51 +0530

    update app.js module

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to jenkins 99999.
+          Welcome to jenkins.
         </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
